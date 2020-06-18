c5d2e9aa5e46aab036c48b435a46cb6ac2a71446
최재훈 <jaychoi@MacBook-Pro.hsd1.mi.comcast.net>
2020-06-18 16:17:51 -0400

    Array_Ds.C#

---
+++ HackerRank/Data Structure/Array_DS.cs
@@ -0,0 +1,46 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+namespace HackerRank
+{
+    public class Array_DS
+
+    {
+        public Array_DS()
+        {
+        }
+
+        static int[] reverseArray_mine(int[] a) {
+            int[] b = a;
+            Array.Reverse(b);
+            return b;
+            
+        }
+       
+        static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int arrCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            int[] res = reverseArray_mine(arr);
+            Array.ForEach(res, Console.WriteLine);
+
+            /*textWriter.WriteLine(string.Join(" ", res));
+
+            textWriter.Flush();
+            textWriter.Close();*/
+        }
+    }
+}
