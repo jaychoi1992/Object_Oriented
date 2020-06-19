2373218ffbf4ea7d7ff568d2fdb79a30aac734af
최재훈 <jaehoonchoi1992@gmail.com>
2020-06-19 19:02:53 -0400

    C#

---
+++ HackerRank/Data Structure/DynamicArray.cs
@@ -0,0 +1,74 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+namespace HackerRank
+{
+    public class DynamicArray
+    {
+        public DynamicArray()
+        {
+        }
+        public static List<int> dynamicArray(int n, List<List<int>> queries)
+        {
+            int lastAnswer = 0;
+            int loc = 0;
+            List<List<int>> seq = new List<List<int>>(n);
+            List<int> last = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                List<int> temp = new List<int>();
+                seq.Add(temp);
+            }
+            foreach (List<int> query in queries)
+            {
+                loc = (query[1] ^ lastAnswer) % n;
+                if (query[0] == 1)
+                {
+                    seq[loc].Add(query[2]);
+                }
+                else
+                {
+                    int size = seq[loc].Count;
+                    lastAnswer = seq[loc][query[2] % size];
+                    last.Add(lastAnswer);
+                }
+            }
+            return last;
+
+        }
+        public static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int q = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<List<int>> queries = new List<List<int>>();
+
+            for (int i = 0; i < q; i++)
+            {
+                queries.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(queriesTemp => Convert.ToInt32(queriesTemp)).ToList());
+            }
+
+            List<int> result = DynamicArray.dynamicArray(n, queries);
+            Console.Write(result.ToString());
+            //textWriter.WriteLine(String.Join("\n", result));
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
---
+++ HackerRank/Data Structure/TwoDArray.cs
@@ -0,0 +1,59 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+namespace HackerRank
+{
+    public class TwoDArray
+    {
+        public TwoDArray()
+        {
+        }
+        static int hourglassSum(int[][] arr)
+        {
+            int maxval = int.MinValue;
+            for (int i = 0; i < 4; ++i)
+            {
+                for (int j = 1; j < 5; ++j)
+                {
+                    int val;
+                    val = arr[i][j-1] + arr[i][j] + arr[i][j+1] + arr[i+1][j] + arr[i+2][j-1] + arr[i+2][j] + arr[i+2][j+1];
+                    if (maxval < val)
+                    {
+                        maxval = val;
+                    }
+                }
+            }
+            return maxval;
+        }
+        static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int[][] arr = new int[6][];
+
+            for (int i = 0; i < 6; i++)
+            {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+
+            int result = hourglassSum(arr);
+            Console.Write(result);
+           
+
+          //  textWriter.WriteLine(result);
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
